2df9bb364f7005614c877efaaa0a05ba745943b7
Akm Elias <akmelias11@gmail.com>
2019-09-17 11:26:14 -0700

    final commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,91 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {RouterModule, Routes} from '@angular/router';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {
+  MatInputModule,
+  MatPaginatorModule,
+  MatProgressSpinnerModule,
+  MatSortModule,
+  MatTableModule,
+  MatIconModule,
+  MatButtonModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatGridListModule,
+  MatListModule,
+  MatButtonToggleModule,
+  MatMenuModule } from '@angular/material';
+import {AboutPageComponent} from './about-page/about-page.component';
+import {HeaderComponent} from './header/header.component';
+import {LogoHeaderComponent} from './logo-header/logo-header.component';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {SignupComponent} from './signup/signup.component';
+import {IndexComponent} from './home-page/index/index.component';
+
+
+// Angular Routes.
+const appRoutes: Routes = [
+  {
+    path: 'index',
+    component: IndexComponent,
+    data: { title: 'Index  Page' }
+  },
+  {
+    path: '',
+    redirectTo: 'about',
+    pathMatch: 'full'
+  },
+  {
+    path: 'about',
+    component: AboutPageComponent,
+    data: { title: 'About TechBook' }
+  },
+  {
+    path: 'signup',
+    component: SignupComponent,
+    data: { title: 'Sign Up' }
+  },
+
+
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutPageComponent,
+    HeaderComponent,
+    LogoHeaderComponent,
+    SignupComponent,
+    IndexComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatProgressSpinnerModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatGridListModule,
+    MatListModule,
+    MatButtonToggleModule,
+    MatMenuModule,
+    FlexLayoutModule
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
